500dfe0ebbebf95f2432ef69588c8e99bd56d667
JaxylViernes <144340288+JaxylViernes@users.noreply.github.com>
2025-11-17 16:34:02 +0200

    New Template Integrations

--- src/utils/TemlplateNavigator.tsx
+++ src/utils/TemlplateNavigator.tsx
@@ -9,6 +9,7 @@ export const getTemplateRoute = (templateId: number, projectId: number) => {
       return `/project/${projectId}/texttypingtemplate`;
     case 3: // Bar Graph
       return `/project/${projectId}/bargraph`;
+    
     case 4: // KPI Flip Cards
       return `/project/${projectId}/kpiflipcards`;
     case 5: // Curve Line Trend
@@ -25,6 +26,16 @@ export const getTemplateRoute = (templateId: number, projectId: number) => {
       return `/project/${projectId}/redditvideo`;
     case 11: // Story Narration
       return `/project/${projectId}/storytelling`;
+    case 12: // Kinetic Text
+      return `/project/${projectId}/kinetictext`;
+    case 13: // Neon Flicker
+      return `/project/${projectId}/neonflicker`;
+    case 14: // Heatmap
+      return `/project/${projectId}/heatmap`;
+    case 15: // Flip Cards
+      return `/project/${projectId}/flipcards`;
+    case 16: // Logo Animation
+      return `/project/${projectId}/logoanimation`;
     default:
       return `/project/${projectId}/unknown`;
   }
